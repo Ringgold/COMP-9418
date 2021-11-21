21e6802d5fea1f3c00c9a53137706fe6b90b9a0f
KarlLie <32243478+dmitrycdz@users.noreply.github.com>
2021-11-21 09:26:39 +0800

    rework start and some dict in solution

--- Assignment2/example_solution.py
+++ Assignment2/example_solution.py
@@ -50,45 +50,41 @@ state = [0] * 40
 state.append(20)
 state = np.array(state)
 
-# index_r is the dictionary storing each room and its corresponding index of state, and index_l is
-# the dictionary storing each light and its corresponding index of state.
-index_r = {}
-index_l = {}
-start = 0
+# dict room store the relationship between list position in state and the name of the room
+room = {}
+
 room_list = [
     'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9', 'r10', 
     'r11', 'r12', 'r13', 'r14', 'r15', 'r16', 'r17', 'r18', 'r19', 'r20', 
     'r21', 'r22', 'r23', 'r24', 'r25', 'r26', 'r27', 'r28', 'r29', 'r30', 
     'r31', 'r32', 'r33', 'r34', 'r35', 'c1', 'c2', 'c3', 'c4', 'o1', 'outside'
 ]
-for r in room_list:
-    index_r[r] = start
-    if start < 35:
-        index_l['lights' + str(start + 1)] = start
-    start += 1
+
+for i in range(len(room_list)):
+    room[room_list[i]] = i
+
 
 tran_matrix_data = pd.read_csv('tran_matrix.csv')
-tran_matrix = {}
-tran_matrix['t1'] = []
-tran_matrix['t2'] = []
+tran_matrix = {'t1': [], 't2': []}
 
 for line in tran_matrix_data:
-    if line[-2:] == 't1':
+    if 't1' in line:
         tran_matrix['t1'].append(list(tran_matrix_data[line]))
-    elif line[-2:] == 't2':
+    elif 't2' in line:
         tran_matrix['t2'].append(list(tran_matrix_data[line]))
 
-for l in tran_matrix.keys():
-    tran_matrix[l] = np.array(tran_matrix[l])
+for t in tran_matrix.keys():
+    tran_matrix[t] = np.array(tran_matrix[t])
 
 def get_action(sensor_data):
-    # declare state as a global variable so it can be read and modified within this function
+    # declare these staff as global so they can be a part of the function
     global state
     global tran_matrix
-    global index_r
-    global index_l
+    global room
     global th
 
+    # multiple the state with transition matrix to get the next state
+    # before adjust with sensors and robots
     if int(sensor_data['time'].hour) < 17:
         state = state @ tran_matrix['t1']
     elif int(sensor_data['time'].hour) == 17 and int(sensor_data['time'].minute) < 30:
@@ -96,7 +92,7 @@ def get_action(sensor_data):
     else:
         state = state @ tran_matrix['t2']
 
-    # adjustment for values of reliable sensors
+    # adjustment with room sensors
     sensor_list = [["reliable_sensor1", "r16", 0.963, 0.009],
                    ["reliable_sensor2", "r5", 0.706, 0.001],
                    ["reliable_sensor3", "r25", 0.902, 0.014],
@@ -108,11 +104,12 @@ def get_action(sensor_data):
                    ]
 
     for slist in sensor_list:
-        if sensor_data[slist[0]] == "motion" and state[index_r[slist[1]]] < th:
-            state[index_r[slist[1]]] = slist[2]
-        if sensor_data[slist[0]] == "no motion" and state[index_r[slist[1]]] > th:
-            state[index_r[slist[1]]] = slist[3]
+        if sensor_data[slist[0]] == "motion" and state[room[slist[1]]] < th:
+            state[room[slist[1]]] = slist[2]
+        if sensor_data[slist[0]] == "no motion" and state[room[slist[1]]] > th:
+            state[room[slist[1]]] = slist[3]
 
+    # adjustment with door sensors
     door_list = [["door_sensor1", "r8", "r9", 0.390, 0.609],
                  ["door_sensor2", "c1", "c2", 0.940, 0.703],
                  ["door_sensor3", "r26", "r27", 0.514, 0.649],
@@ -120,23 +117,28 @@ def get_action(sensor_data):
                  ]
     for dlist in door_list:
         if sensor_data[dlist[0]] and int(sensor_data[dlist[0]]) > 0:
-            if state[index_r[dlist[1]]] < th:
-                state[index_r[dlist[1]]] = dlist[3]
-            if state[index_r[dlist[2]]] < th:
-                state[index_r[dlist[2]]] = dlist[4]
+            if state[room[dlist[1]]] < th:
+                state[room[dlist[1]]] = dlist[3]
+            if state[room[dlist[2]]] < th:
+                state[room[dlist[2]]] = dlist[4]
 
-    # robot sensors
+    # robot
     for r in ['robot1', 'robot2']:
         if sensor_data[r]:
-            room = sensor_data[r].split(',')[0].strip('(').strip("'")
+            position = sensor_data[r].split(',')[0].strip('(').strip("'")
             people = sensor_data[r].split(',')[1].strip(')')
-            state[index_r[room]] = int(people)
+            state[room[position]] = int(people)
 
+    # generate the actions based on states
+    # if the number of people in a room is more than the threshold
+    # then turn on the light
     actions_dict = {}
-    for key in index_l:
-        if state[index_l[key]] > th:
-            actions_dict[key] = 'on'
+
+    for r in range(0, 35):
+        light = "lights" + str(r+1)
+        if state[r] > th:
+            actions_dict[light] = 'on'
         else:
-            actions_dict[key] = 'off'
-    
+            actions_dict[light] = 'off'
+
     return actions_dict
